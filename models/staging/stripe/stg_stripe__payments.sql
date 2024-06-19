4eb097e7aa405a270df679b36241908e36e4686f
amolgaikwad <amol.gaikwad.31@gmail.com>
2024-06-19 17:01:12 +0530

    Create new fact orders table and rename customers to dimension customers table

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,6 +3,7 @@ select
     orderid as order_id,
     paymentmethod,
     status,
-    amount,
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
     created
 from {{ source('stripe', 'payment')}}
